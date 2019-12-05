26f29b7ba3528a1b3047a13da48c9bdb94c49ab6
blindbatman <emlundaj@gmail.com>
2019-12-04 20:36:27 -0700

    after stretch

--- index.js
+++ index.js
@@ -311,8 +311,15 @@ const argTimesTwo = num => num * 2;
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
  */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(num) {
+  var car = {
+    odometer: num,
+    drive: function(distance) {
+      this.odometer = this.odometer + distance;
+      return this.odometer;
+    }
+  };
+  return car;
 }
 
 /// ////// END OF CHALLENGE /////////
